4b996b07d138e02891279bac7260e9bd1ab26d91
lovelyhyeony <dmlgus1222@naver.com>
2021-01-02 03:31:36 +0900

    netflix clone coding project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 🎥 Netflix clone coding project with ES7
 
 - 2021.01.01 ~
-- 참고 자료 : Clever Programmer
+- 참고 자료 : [Clever Programmer](https://www.youtube.com/watch?v=XtMThy8QKqU&t=1378s)
 
 ### Library
 
--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Row from "./component/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix 오리지널" />
-      <Row title="지금 뜨는 콘텐츠" />
+      <Row title="Netflix 오리지널" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="지금 뜨는 콘텐츠" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
