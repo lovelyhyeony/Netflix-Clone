5b283a2c214d267f3c8bae8425c13bc9388b6160
sun5066 <astjr4434@naver.com>
2021-01-02 23:51:32 +0900

    netflix clone coding project

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import Banner from "./component/Banner";
 import Row from "./component/Row";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix 오리지널" fetchUrl={requests.fetchNetflixOriginals} />
+      {/** nav */}
+      <Banner />
+      <Row
+        title="Netflix 오리지널"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="지금 뜨는 콘텐츠" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="액션 영화" fetchUrl={requests.fetchActionMovies} />
+      <Row title="코미디 영화" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="호러 영화" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="로맨스 영화" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="다큐멘터리 영화" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/component/Row.js
+++ netflix-clone/src/component/Row.js
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "../css/Row.css";
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+      <div className="row-posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row-poster ${isLargeRow && "row-posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
